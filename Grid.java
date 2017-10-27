53da1e3de49249471cbad7d106e0bbd8b320eab0
Hassib Ashouri <hassib291@hotmail.com>
2017-10-27 00:16:41 -0700

    Documentation is added. Classes is tested.

--- Grid.java
+++ Grid.java
@@ -2,19 +2,19 @@ package sudoku;
 
 import java.util.*;
 
-
+/**
+ * This class represents a 9X9 sudoku grid.
+ */
 public class Grid 
 {
 	private int[][]						values;
-	
 
-	//
-	// DON'T CHANGE THIS.
-	//
-	// Constructs a Grid instance from a string[] as provided by TestGridSupplier.
-	// See TestGridSupplier for examples of input.
-	// Dots in input strings represent 0s in values[][].
-	//
+
+
+	/** 
+	 * ctor to create a grid instance.
+	 * @param rows : array that holds a the rows as strings with the char '.' repesenting an empty slot.
+	 */
 	public Grid(String[] rows)
 	{
 		values = new int[9][9];
@@ -32,9 +32,24 @@ public class Grid
 	}
 	
 	
-	//
-	// DON'T CHANGE THIS.
-	//
+	
+	/**
+	 * ctor of an instance of Grid using another grid object.
+	 * @param src : A Grid object.
+	 */
+	Grid(Grid src)
+	{
+		values = new int[9][9];
+		for (int j=0; j<9; j++)
+			for (int i=0; i<9; i++)
+				values[j][i] = src.values[j][i];
+	}
+	
+
+
+	/**
+	 * Prints the grid as rows of strings with empty slots represented by '.'.
+	 */
 	public String toString()
 	{
 		String s = "";
@@ -54,83 +69,47 @@ public class Grid
 	}
 
 
-	//
-	// DON'T CHANGE THIS.
-	// Copy ctor. Duplicates its source. Youâ€™ll call this 9 times in next9Grids.
-	//
-	Grid(Grid src)
-	{
-		values = new int[9][9];
-		for (int j=0; j<9; j++)
-			for (int i=0; i<9; i++)
-				values[j][i] = src.values[j][i];
-	}
 	
-	
-	//
-	// COMPLETE THIS
-	//
-	//
-	// Finds an empty member of values[][]. Returns an array list of 9 grids that look like the current grid,
-	// except the empty member contains 1, 2, 3 .... 9. Returns null if the current grid is full. Donâ€™t change
-	// â€œthisâ€� grid. Build 9 new grids.
-	// 
-	//
-	// Example: if this grid = 1........
-	//                         .........
-	//                         .........
-	//                         .........
-	//                         .........
-	//                         .........
-	//                         .........
-	//                         .........
-	//                         .........
-	//
-	// Then the returned array list would contain:
-	//
-	// 11.......          12.......          13.......          14.......    and so on     19.......
-	// .........          .........          .........          .........                  .........
-	// .........          .........          .........          .........                  .........
-	// .........          .........          .........          .........                  .........
-	// .........          .........          .........          .........                  .........
-	// .........          .........          .........          .........                  .........
-	// .........          .........          .........          .........                  .........
-	// .........          .........          .........          .........                  .........
-	// .........          .........          .........          .........                  .........
-	//
+	/**
+	 * This method optain the possible 9 suduku grids by filling the first
+	 * empty slot with numbers 1-9.
+	 */
 	public ArrayList<Grid> next9Grids()
 	{		
 		int xOfNextEmptyCell = 0;
 		int yOfNextEmptyCell = 0;
-		char emptySlot = '.';
-		boolean stop = false;
+		int emptySlot = 0 ;
 
-		if(this.isFull() == true)
+		if( this.isFull() == true )
+		{
 			return null;
-
+		}
 
 		// Find x,y of an empty cell.
 
-		for(int row = 0; row < this.values.length && !stop; row++)
+		for( int row = 0; row < this.values.length ; row++ )
 		{
-			for(int col = 0; col < this.values[0].length && !stop; col++)
+			for( int col = 0; col < this.values[0].length ; col++ )
 			{
-				if(this.values[row][col] == emptySlot)
+				if( this.values[row][col] == emptySlot )
 				{
+					// identify the empty slot.
 					xOfNextEmptyCell = col;
 					yOfNextEmptyCell = row;
-					stop = true;
+					// break from the nested loop
+					row = this.values.length;
+					break;
 				}
 			}
 		}
 
 		// Construct array list to contain 9 new grids.
-		ArrayList<Grid> grids = new ArrayList<Grid>();
+		ArrayList<Grid> grids = new ArrayList<>();
 
 		// Create 9 new grids as described in the comments above. Add them to grids.
 		Grid gridInstance;
 
-		for(int num = 1; num <= 9; num++ )
+		for( int num = 1 ; num <= 9 ; num++ )
 		{
 			gridInstance = new Grid(this);
 			gridInstance.values[yOfNextEmptyCell][xOfNextEmptyCell] = num;
@@ -142,68 +121,83 @@ public class Grid
 	}
 	
 	
-	//
-	// COMPLETE THIS
-	//
-	// Returns true if this grid is legal. A grid is legal if no row, column, or
-	// 3x3 block contains a repeated 1, 2, 3, 4, 5, 6, 7, 8, or 9.
-	//
+	/**
+	 * Checks every row, column, and blok of 3X3 for reapeated number.
+	 * @return true : if the grid is legal.
+	 */
 	public boolean isLegal()
 	{
-		int[] checkForRepeatsArray ;
-		// Check every row. If you find an illegal row, return false.
-		for(int row = 0 ; row < this.values.length ; row++ )
+		int[] checkForRepeatsArray ; // holds numbers to be checked for repeats later on.
+
+
+		// Goes through every line and checks for repeats.
+
+		for( int row = 0 ;	row < this.values.length	; row++ )
 		{
 			checkForRepeatsArray = this.values[row];
-			if(this.isThereRepeats(checkForRepeatsArray))
+			// check for repeats
+			if(		this.isThereRepeats(checkForRepeatsArray)		)
 			{
+				assert	!this.isThereRepeats(checkForRepeatsArray)	: "Error: one of the rows has a repeated number." ;				
+				
 				return false;
 			}
 		}
 
-		// Check every column. If you find an illegal column, return false.
-		checkForRepeatsArray = new int[this.values.length];
-		for(int col = 0 ; col < this.values[0].length ; col++ )
+		// Goes through every column
+
+		checkForRepeatsArray = new int[this.values.length]; // initialize the array
+
+		for( int col = 0 ;	col < this.values[ 0 ].length		; col++ )
 		{
 			//add the numbers in one column to one array.
-			for( int row = 0 ; row < this.values.length ; row++ )
+
+			for( int row = 0 ;	row < this.values.length	; row++ )
 			{
-				checkForRepeatsArray[row] = this.values[row][col];
+				checkForRepeatsArray[ row ] = this.values[ row ][ col ];
 			}
+			
 			//check if there is repeats
-			if(this.isThereRepeats(checkForRepeatsArray))
+
+			if(		this.isThereRepeats(checkForRepeatsArray)	)
 			{
+				assert	!this.isThereRepeats(checkForRepeatsArray)	: "Error: one of the columns has a repeated number." ;				
+				
 				return false;
 			}
 			
 		}
 
-		// Check every block. If you find an illegal block, return false.
-		//we treat the sudoku as a grid of blocks in handelling this part.
+		// treat the sudoku as a grid of blocks in handelling this part.
+		
 		int xOfBlock; //from 0 to 2
 		int yOfBlock; //from 0 to 2
-		int temp = 0;
+		int temp = 0; // used as an index when addin the numbers
 
-		for(xOfBlock = 0 ; xOfBlock < 3 ; xOfBlock++ )
+		for( xOfBlock = 0 ;		xOfBlock < 3	; xOfBlock++ )
 		{
-			for( yOfBlock = 0 ; yOfBlock < 3 ; yOfBlock++ )
+			for( yOfBlock = 0 ;		yOfBlock < 3	; yOfBlock++ )
 			{
-				
-				for(int row = 0 + (3 * yOfBlock) ; row < 3 * (yOfBlock + 1) ; row ++)
+				// traverse through the values of each block
+
+				for( int row = 0 + (3 * yOfBlock) ;		row < 3 * (yOfBlock + 1)	; row ++ )
 				{
-					for( int col = 0 + (3 * xOfBlock) ; col < 3 * (xOfBlock + 1) ; col++ )
+					for( int col = 0 + (3 * xOfBlock) ;		col < 3 * (xOfBlock + 1)	; col++ )
 					{
-						checkForRepeatsArray[temp++] = this.values[row][col];
+						checkForRepeatsArray[ temp++ ] = this.values[ row ][ col ];
 					}	
 				}
 
+				// check for repeats in the values of a block
 
-				if(this.isThereRepeats(checkForRepeatsArray))
+				if(		this.isThereRepeats(checkForRepeatsArray)	)
 				{
+					assert	!this.isThereRepeats(checkForRepeatsArray)	: "Error: one of the blocks has a repeated number.";
+					
 					return false;
 				}
 
-				temp = 0;
+				temp = 0; // set the zero to assure proper index the next iteration.
 			}
 		}
 
@@ -212,19 +206,17 @@ public class Grid
 		return true;
 	}
 
-	
-	//
-	// COMPLETE THIS
-	//
-	// Returns true if every cell member of values[][] is a digit from 1-9.
-	//
+	/**
+	 * checks if the grid is full.
+	 * @return true : if the grid is full.
+	 */
 	public boolean isFull()
 	{
-		for(int row = 0 ; row < this.values.length ; row++ )
+		for( int row = 0 ;	row < this.values.length	; row++ )
 		{
-			for( int col = 0 ; col < this.values[0].length ; col++ )
+			for( int col = 0 ;	col < this.values[ row ].length		; col++ )
 			{
-				if(this.values[row][col] == 0)
+				if(		this.values[ row ][ col ] == 0	)
 				{
 					return false;
 				}
@@ -234,21 +226,20 @@ public class Grid
 		return true;
 	}
 	
-
-	//
-	// COMPLETE THIS
-	//
-	// Returns true if x is a Grid and, for every (i,j), 
-	// x.values[i][j] == this.values[i][j].
-	//
+	/**
+	 * Checks if two grid instances have the same numbers.
+	 * @return true : if instances have the sam numbers.
+	 */
+	@Override
 	public boolean equals(Object x)
 	{
 		Grid that = (Grid) x;
-		for( int row = 0 ; row < this.values.length ; row++ )
+
+		for( int row = 0 ;	row < this.values.length	; row++ )
 		{
-			for( int col = 0 ; col < this.values[row].length ; col++ )
+			for( int col = 0 ;	col < this.values[ row ].length		; col++ )
 			{
-				if(this.values[row][col] != that.values[row][col])
+				if(		this.values[ row ][ col ] != that.values[ row ][ col ]	)
 				{
 					return false;
 				}
@@ -256,21 +247,27 @@ public class Grid
 		}
 
 		return true;
-
 	}
 
-	private boolean isThereRepeats(int[] arr)
+	/**
+	 * Checks if an array has a repeted numbers
+	 * @return true : if the array has repeated numbers.
+	 */
+	private boolean isThereRepeats( int[] arr )
 	{
 		int temp;
+
 		for(int i = 0 ; i < arr.length ; i++ )
 		{
-			temp = arr[i];
-			if(temp != 0)
+			temp = arr[ i ];
+			if(		temp != 0	)
 			{
-				for(int j = i + 1 ; j < arr.length ; j ++)
+				for(int j = i + 1 ;		j < arr.length		; j ++)
 				{
-					if( temp == arr[j] )
+					if(		temp == arr[ j ]	)
+					{	
 						return true;
+					}
 				}
 			}
 		

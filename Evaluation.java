be3f1090d8bac674a0a7e695be6a537eba999f45
Hassib Ashouri <hassib291@hotmail.com>
2017-10-28 16:15:34 -0700

    Added documentation for the stated of the grid.

--- Evaluation.java
+++ Evaluation.java
@@ -1,6 +1,19 @@
 package sudoku;
-
+/**
+ * This enum provides ways to describe the legitemecy and status of the grid.
+ */
 enum Evaluation 
 {
-	  ACCEPT, ABANDON, CONTINUE;
+	/**
+	 * The grid is legal, full. It could be a solution.
+	 */
+	ACCEPT,
+	/**
+	 * The grid is not legal.
+	 */
+	ABANDON, 
+	/**
+	 * The drid is legal with empty slots.
+	 */
+	CONTINUE;
 }
